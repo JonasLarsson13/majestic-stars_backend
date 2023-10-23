8c366febe1dc8e76c247ce5a61a0bfc7880fb3e6
Jonas Larsson <larsson2p0@MacBook-Pro-som-tillhor-Jonas.local>
2023-10-23 12:51:51 +0200

    backend template done

---
+++ src/responses/index.js
@@ -0,0 +1,20 @@
+export function sendResponse(statusCode, response) {
+  return {
+    statusCode,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(response),
+  };
+}
+export function sendError(statusCode, message) {
+  return {
+    statusCode: statusCode || 500,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      message: message || "Internal Server Error",
+    }),
+  };
+}
