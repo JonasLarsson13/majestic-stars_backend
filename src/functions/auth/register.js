3c0d1864ff72690fb296ec3696ae9f899f706c9f
Jonas Larsson <larsson2p0@MacBook-Pro-som-tillhor-Jonas.local>
2023-10-24 15:50:37 +0200

    hej

--- src/functions/auth/register.js
+++ src/functions/auth/register.js
@@ -1,64 +1,59 @@
 import bcrypt from "bcrypt";
-import { localTest } from '../../services/connect.js'
-import { model, mongoose, Schema } from 'mongoose'
+import { localTest } from "../../services/connect.js";
+import { mongoose, Schema } from "mongoose";
+import { sendError, sendResponse } from "../../responses/index.js";
 
-var UserSchema = new Schema({
+var UserSchema = new Schema(
+  {
     email: {
       type: String,
       unique: true,
       lowercase: true,
       trim: true,
-      required: true
+      required: true,
     },
     hash_password: {
-      type: String
+      type: String,
     },
-  }, { autoCreate: true });
-  
-  UserSchema.methods.comparePassword = function(password) {
-    return bcrypt.compareSync(password, this.hash_password);
-  };
-  
-  mongoose.model('User', UserSchema, 'Users');
+  },
+  { autoCreate: true }
+);
 
-const User = mongoose.model('User');
+UserSchema.methods.comparePassword = function (password) {
+  return bcrypt.compareSync(password, this.hash_password);
+};
+
+mongoose.model("User", UserSchema, "Users");
+
+const User = mongoose.model("User");
 
 async function registration(req, res) {
-    try {
-        const conn = await localTest();
-        const newUser = new User(req.body);
-        newUser.hash_password = bcrypt.hashSync(req.body.password, 10);
-        
-        const savedUser = await newUser.save();
-        savedUser.hash_password = undefined;
-        
-        return res.json(savedUser);
-    } catch (err) {
-        return res.status(400).send({
-            message: err.message
-        });
-    }
+  try {
+    const conn = await localTest();
+    const newUser = new User(req.body);
+    newUser.hash_password = bcrypt.hashSync(req.body.password, 10);
+
+    const savedUser = await newUser.save();
+    savedUser.hash_password = undefined;
+
+    return res.json(savedUser);
+  } catch (err) {
+    return res.status(400).send({
+      message: err.message,
+    });
+  }
 }
 
 export async function handler(event, context) {
-    try {
-        
-      // Handle the actual POST request
-      console.log(event)
-      const requestBody = JSON.parse(event);
-      const userReg = await registration(event);
-      console.log(userReg);
-      return {
-        statusCode: 200,
-        headers: commonHeaders,
-        body: JSON.stringify({ chatresult }),
-      };
-    } catch (error) {
-      console.error(error);
-      return {
-        statusCode: 500,
-        headers: commonHeaders,
-        body: JSON.stringify({ error: 'Internal Server Error' }),
-      };
-    }
+  try {
+    // Handle the actual POST request
+    console.log(event);
+    const requestBody = JSON.parse(event);
+    const userReg = await registration(event);
+    console.log(userReg);
+    return sendResponse(200, { message: "User registered successfully" });
+  } catch (error) {
+    console.error(error);
+    return sendError(500, "something went wrong");
   }
+}
