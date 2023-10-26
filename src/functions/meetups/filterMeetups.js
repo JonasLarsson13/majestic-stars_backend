2b7c5246cc23a702dbde67de3ef80ac659c4ff6f
Jonas Larsson <larsson2p0@MacBook-Pro-som-tillhor-Jonas.local>
2023-10-26 10:51:30 +0200

    filter function working

---
+++ src/functions/meetups/filterMeetups.js
@@ -0,0 +1,51 @@
+import connectDB from "../../services/db.js";
+import Meetup from "../../models/meetupModel.js";
+import { sendResponse, sendError } from "../../responses/index.js";
+
+export const handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  try {
+    await connectDB();
+
+    const { fromDate, toDate, city, category } = event.queryStringParameters;
+
+    console.log("Query Parameters:", { fromDate, toDate, city, category });
+
+    if (fromDate && toDate && new Date(fromDate) > new Date(toDate)) {
+      return sendError(400, { message: "Invalid date range" });
+    }
+
+    const filters = {};
+
+    const toDateObj = new Date(toDate);
+    toDateObj.setDate(toDateObj.getDate() + 1);
+    if (fromDate && toDate) {
+      filters.$or = [
+        { startDate: { $gte: new Date(fromDate), $lte: toDateObj } },
+        { endDate: { $gte: new Date(fromDate), $lte: toDateObj } },
+      ];
+    }
+
+    if (city) {
+      filters.city = city;
+    }
+
+    if (category) {
+      filters.tags = category;
+    }
+
+    console.log("Filter Criteria:", filters);
+
+    const filteredMeetups = await Meetup.find(filters);
+
+    if (filteredMeetups.length === 0) {
+      return sendResponse(404, { message: "No meetups found" });
+    }
+
+    return sendResponse(200, filteredMeetups);
+  } catch (error) {
+    console.error("Error:", error);
+    return sendError(500, { message: "Internal server error" });
+  }
+};
