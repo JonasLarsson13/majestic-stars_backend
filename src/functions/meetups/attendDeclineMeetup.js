6f4f2c7ca21c512f115c571fd9b11a130f53d454
Jonas Larsson <larsson2p0@MacBook-Pro-som-tillhor-Jonas.local>
2023-10-31 10:49:10 +0100

    small changes

--- src/functions/meetups/attendDeclineMeetup.js
+++ src/functions/meetups/attendDeclineMeetup.js
@@ -1,31 +1,32 @@
+import { middyfy } from "@/services/middify";
 import Meetup from "../../models/meetupModel.js";
 import { sendResponse, sendError } from "../../responses/index.js";
-import { middyfy } from "@/services/middify";
 import { checkPermission } from "../../middleware/permission.js";
 
 const attendDeclineMeetup = async (event) => {
   const { meetupId } = event.pathParameters;
-
   try {
     const meetup = await Meetup.findById(meetupId);
     if (!meetup) {
       return sendResponse(404, { message: "Meetup not found" });
     }
-  const userIndex = meetup.participants.findIndex((participant) => participant.equals(event.user._id));
+    const userIndex = meetup.participants.findIndex(
+      (participant) => participant === event.user._id.toString()
+    );
 
-  if (userIndex !== -1) {
-    meetup.participants.splice(userIndex, 1);
-  } else {
-    meetup.participants.push(event.user._id);
-  }
+    if (userIndex !== -1) {
+      meetup.participants.splice(userIndex, 1);
+    } else {
+      meetup.participants.push(event.user._id.toString());
+    }
 
-  await meetup.save();
+    await meetup.save();
 
-    return sendResponse(200, {success: true});
+    return sendResponse(200, { success: true });
   } catch (error) {
     console.error(error);
     return sendError(500, "Internal server error");
   }
 };
 
-export const handler = middyfy(attendDeclineMeetup).use(checkPermission)
+export const handler = middyfy(attendDeclineMeetup).use(checkPermission);
